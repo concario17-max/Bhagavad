3eab1d0c90598ab26dbcab52d681853cc51b6701
concario17-max <concario17@gmail.com>
2026-02-21 10:36:05 +0900

    chore: trigger deployment build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// Deployment trigger: 2026-02-21
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
