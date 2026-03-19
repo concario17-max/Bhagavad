986c71ad927beb154c0f484d9f31d4011d20e5c8
concario17-max <concario17@gmail.com>
2026-03-19 16:42:18 +0900

    fix: keep header action pinned

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,12 +20,11 @@ const Header = ({
     className = ''
 }: HeaderProps) => {
     const {
-        isDesktopCommentaryPanelOpen,
         toggleSidebar
     } = useUI();
 
     const leftPanelWidth = showSidebarToggle ? 400 : 0;
-    const rightPanelWidth = isDesktopCommentaryPanelOpen ? 400 : 0;
+    const rightPanelWidth = rightContent ? 400 : 0;
 
     const headerContent = (
         <div className="flex h-[72px] items-center justify-between px-4 lg:px-6">
